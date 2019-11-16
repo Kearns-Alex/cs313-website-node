ae5491d972d95a374b1fd5f82161f95f0e459fdc
Alex Kearns <alex.j.kearns@gmail.com>
2019-11-16 00:37:31 -0500

    Reverting

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-// use any file in this folder
-// look here for a match
   .use(express.static(path.join(__dirname, 'public')))
-// setting up the views
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-// specific page request
-//* ROOT
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
