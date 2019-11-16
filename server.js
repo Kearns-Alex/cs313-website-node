beb1f70ccb293e4e336e5c6b8095d28983494965
Alex Kearns <alex.j.kearns@gmail.com>
2019-11-16 00:22:04 -0500

    Update for what I need

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+// use any file in this folder
+// look here for a match
   .use(express.static(path.join(__dirname, 'public')))
+// setting up the views
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+// specific page request
+//* ROOT
+  .get('/', function(req, res) {
+    res.render('pages/index');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
