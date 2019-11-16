8993b582d8b47ab35fb85a541edab323381d9289
Alex Kearns <alex.j.kearns@gmail.com>
2019-11-16 18:24:35 -0500

    page is at our setup for the files

--- server.js
+++ server.js
@@ -4,7 +4,27 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  //* Setting up the views 
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  //* Setting up all of the web pages
+  //* HOME PAGE
+  .get('/', function(req, res) {
+    console.log('Received a request for /');
+
+    res.render('pages/index')
+  })
+  //* ASSIGNMENTS PAGE
+  .get('/assignments', function(req, res) {
+    console.log('Received a request for /assignments');
+
+    res.render('pages/assignments')
+  })
+  //* POSTAL PAGE
+  .get('/postal', function(req, res) {
+    console.log('Received a request for /postal');
+
+    res.render('pages/postal')
+  })
+  //* Set up the servers app and port
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
